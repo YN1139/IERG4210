1730aa1be8d71a1d8c77add8c1268a6646fde14a
Y.N <yanhyng0227@gmail.com>
2025-03-08 23:52:19 +0800

    test for image naming

--- server/server.mjs
+++ server/server.mjs
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 const corsOptions = {
@@ -7,8 +11,41 @@ const corsOptions = {
   credentials: true, // access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+const db = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "shop27-admin",
+  password: "mypass",
+  database: "shop27",
+});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}${ext}`);
+  },
+});
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (
+      file.mimetype !== "image/jpg" &&
+      file.mimetype !== "image/png" &&
+      file.mimetype !== "image/gif"
+    ) {
+      return cb(new Error("Only jpg, png and gif are allowed"));
+    }
+    if (file.size > 10000000) {
+      return cb(new Error("File size must be less than or is equal to 10MB"));
+    }
+    cb(null, true);
+  },
+});
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Update the database after receriving the form submission from the client
 app.post("/admin/add-product", upload.single("image"), async (req, res) => {
@@ -18,13 +55,22 @@ app.post("/admin/add-product", upload.single("image"), async (req, res) => {
 
     const sql =
       "INSERT INTO products (catid, name, price, description, image) VALUES (?, ?, ?, ?, ?)";
-    const result = await db.query(sql, [
+    const [result] = await db.query(sql, [
       catid,
       name,
       price,
       description,
       imagePath,
     ]);
+    const pid = result.insertId;
+
+    if (imagePath) {
+      const newImagePath = `uploads/${pid}${path.extname(imagePath)}`;
+      fs.renameSync(imagePath, newImagePath);
+      const updateSql = "UPDATE products SET image = ? WHERE pid = ?";
+      await db.query(updateSql, [newImagePath, pid]);
+    }
+
     res.status(200).send({ message: "Product added successfully!" });
   } catch (error) {
     res.status(400).send(error);
