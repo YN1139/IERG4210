066226c1295e210145dbe9c741c90d526c8aa7f4
Y.N <yanhyng0227@gmail.com>
2025-03-09 12:18:04 +0800

    update index.js for dynamically update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const API = "http://13.238.18.138:3000";
 
 document.addEventListener("DOMContentLoaded", function () {
   fetchCategories();
-  //fetchProducts();
+  fetchProducts();
 });
 
 async function fetchCategories() {
@@ -21,6 +21,31 @@ async function fetchCategories() {
         });
         li.appendChild(a);
         categoryList.appendChild(li);
+      });
+    });
+}
+
+async function fetchProducts(catid = null) {
+  fetch(API + "/api/products/" + catid)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const productList = document.querySelector(".productList");
+      productList.innerHTML = "";
+      data.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.className = "product";
+        productDiv.innerHTML = `
+        <a href="product/${product.pid}">
+            <img
+              src="${product.image}"
+              alt="${product.name}"
+            />
+            <div class="productInfo">${product.name}<br>HKD $${product.price}</div>
+          </a>
+          <button data-pid="${product.pid}">Add to cart</button>
+        `;
+        productList.appendChild(productDiv);
       });
     });
 }
