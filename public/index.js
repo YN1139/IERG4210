1108015ae1a38749cbd30d15d5f3954b41f514e9
Y.N <yanhyng0227@gmail.com>
2025-03-10 01:48:22 +0800

    update for file structure and cart.js for easy manangement

---
+++ public/index.js
@@ -0,0 +1,76 @@
+const API = "http://13.238.18.138:3000";
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchCategories();
+  fetchAllProducts();
+});
+
+async function fetchCategories() {
+  fetch(API + "/api/cat")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const categoryList = document.querySelector(".categoryMenu ul");
+      data.forEach((category) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.textContent = category.name;
+        a.addEventListener("click", function () {
+          fetchProducts(category.catid);
+        });
+        li.appendChild(a);
+        categoryList.appendChild(li);
+      });
+    });
+}
+
+async function fetchAllProducts() {
+  fetch(API + "/api/prod/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const productList = document.querySelector(".productList");
+      productList.innerHTML = "";
+      data.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.className = "product";
+        productDiv.innerHTML = `
+          <a href="product.html?pid=${product.pid}">
+              <img
+                src="${product.image}"
+                alt="${product.name}"
+              />
+              <div class="productInfo">${product.name}<br>HKD $${product.price}</div>
+            </a>
+            <button class="add-to-cart" data-pid="${product.pid}">Add to cart</button>
+          `;
+        productList.appendChild(productDiv);
+      });
+    });
+}
+
+async function fetchProducts(catid = null) {
+  fetch(API + "/api/products/" + catid)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const productList = document.querySelector(".productList");
+      productList.innerHTML = "";
+      data.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.className = "product";
+        productDiv.innerHTML = `
+        <a href="product.html?pid=${product.pid}">
+            <img
+              src="${product.image}"
+              alt="${product.name}"
+            />
+            <div class="productInfo">${product.name}<br>HKD $${product.price}</div>
+          </a>
+          <button class=add-to-cart data-pid="${product.pid}">Add to cart</button>
+        `;
+        productList.appendChild(productDiv);
+      });
+    });
+}
--- public/product.js
+++ public/product.js
@@ -1,6 +1,37 @@
 const API = "http://13.238.18.138:3000";
 
-class ShoppingCart {
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pid = urlParams.get("pid");
+  fetchProduct(pid);
+});
+
+async function fetchProduct(pid) {
+  fetch(API + "/api/product/" + pid)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const product = data[0];
+
+      const productContainer = document.querySelector(".item");
+      productContainer.innerHTML = `
+        <img 
+          src="server/${product.image}"
+          alt="${product.name}"
+        >
+        <section class="itemInfo">
+          <h2>${product.name}</h2>
+          <div class="itemDescription">
+            ${product.description}
+          </div>
+          <p>HKD $${product.price}</p>
+          <button class="add-to-cart" data-pid="${product.pid}">Add to Cart</button>
+        </section>
+      `;
+    });
+}
+
+/* class ShoppingCart {
   constructor() {
     this.items = new Map();
     this.loadFromStorage();
@@ -143,9 +174,4 @@ class ShoppingCart {
     }
   }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (!window.cart) {
-    window.cart = new ShoppingCart();
-  }
-});
+ */
