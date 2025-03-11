1e3a4cd338541435971134ae5e8e691756425d74
Y.N <yanhyng0227@gmail.com>
2025-03-11 22:23:42 +0800

    debug for breadcrumb

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ async function fetchBreadcrumb(catid = null) {
     .then((data) => {
       console.log(data);
       const breadcrumb = document.querySelector(".breadcrumb ol");
-      const crumb = breadcrumb.querySelector("li:not(:first-child)");
+      const crumb = breadcrumb.querySelectorAll("li:not(:first-child)");
       crumb.forEach((li) => li.remove());
       data.forEach((category) => {
         const li = document.createElement("li");
