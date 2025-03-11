72d9dad64315d816845af7294820172f81feadff
Y.N <yanhyng0227@gmail.com>
2025-03-11 22:51:42 +0800

    debug for breadcrumb

--- public/product.js
+++ public/product.js
@@ -1,4 +1,4 @@
-//const API = "http://13.238.18.138:3000";
+const API = "http://13.238.18.138:3000";
 
 document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 async function fetchProduct(pid) {
+  fetchBreadcrumbProduct(pid);
   fetch(API + "/api/product/" + pid)
     .then((response) => response.json())
     .then((data) => {
@@ -31,12 +32,31 @@ async function fetchProduct(pid) {
     });
 }
 
+async function fetchBreadcrumb(catid = null) {
+  fetch(API + "/api/category/" + catid)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const breadcrumb = document.querySelector(".breadcrumb ol");
+      const crumb = breadcrumb.querySelectorAll("li:not(:first-child)"); //selecr all li except the first one
+      crumb.forEach((li) => li.remove()); //remove them
+      data.forEach((category) => {
+        const li = document.createElement("li");
+        li.className = "crumb";
+        li.innerHTML = `
+        <a href="#">${category.name}</a>
+      `;
+        breadcrumb.appendChild(li);
+      });
+    });
+}
+
 async function fetchBreadcrumbProduct(pid) {
   fetch(API + "/api/product/" + pid)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      fetchBreadcrumb(data.catid);
+      //fetchBreadcrumb(data.catid);
       const li = document.createElement("li");
       li.className = "crumb";
       li.innerHTML = `
