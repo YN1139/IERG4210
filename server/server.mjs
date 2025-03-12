cb87ff85cb23244e2c369fdbfb525b89b6f2b0e8
Y.N <yanhyng0227@gmail.com>
2025-03-12 13:41:17 +0800

    minor bug fix

--- server/server.mjs
+++ server/server.mjs
@@ -54,9 +54,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "../")));
 app.use("/public", express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
-});
+}); */
 
 //Load the categories and products to homepage
 app.get("/api/cat", async (req, res) => {
