c4c746126f65235a0bef3ebaf45ada69d3fabc1b
Y.N <yanhyng0227@gmail.com>
2025-05-01 01:25:46 +0800

    remove unnessecary print function

--- public/users/cart.js
+++ public/users/cart.js
@@ -197,7 +197,7 @@ async function InitStripe() {
   fetch("/api/stripe")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.stripe_cart);
+      //console.log(data.stripe_cart);
       stripe = Stripe(data.stripe_cart);
     });
 }
