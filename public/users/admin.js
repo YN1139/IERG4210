0f89154093881207941bd15dedc45c1a044c0078
Y.N <yanhyng0227@gmail.com>
2025-05-10 01:19:32 +0800

    Refactor admin form to remove unused fields and implement dynamic product listing for deletion

--- public/users/admin.js
+++ public/users/admin.js
@@ -9,8 +9,39 @@ function showForm() {
   const forms = document.querySelectorAll("form");
   const selctedValue = document.getElementById("action").value;
 
+  console.log(selctedValue);
+
   forms.forEach((form) => {
     form.style.display = "none";
   });
   document.getElementById(selctedValue).style.display = "block";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const formValue = document.getElementById("action").value;
+  if (formValue === "delete") {
+    fetch("/api/prod")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const productsList = document.getElementById("products-list");
+        productsList.innerHTML = "";
+
+        data.forEach((product) => {
+          const productDiv = document.createElement("div");
+          productDiv.className = "product";
+
+          const checkbox = document.createElement("input");
+          checkbox.type = "checkbox";
+          checkbox.value = product.pid;
+
+          const productInfo = document.createElement("div");
+          productInfo.textContent = product.pid + "-" + product.name;
+
+          productDiv.appendChild(checkbox);
+          productDiv.appendChild(productInfo);
+          productsList.appendChild(productDiv);
+        });
+      });
+  }
+});
