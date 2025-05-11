5500590f43c9a9a79776ad6214b2b5d960e2f04a
Y.N <yanhyng0227@gmail.com>
2025-05-11 22:33:12 +0800

    Update member panel layout by replacing order container ID, adding footer with project details, and introducing a cart item template. Modify CSS to reflect the new ID for orders container.

---
+++ public/member.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetchOrders();
+});
+
+function fetchOrders() {
+  fetch("/api/orders")
+    .then((response) => response.json())
+    .then(async (data) => {
+      console.log(data);
+      const ordersList = document.getElementById("orders-container");
+      ordersList.innerHTML = "";
+
+      data.forEach((order) => {
+        console.log(order);
+        const box = document.createElement("div");
+        box.className = "order-box";
+        const idDiv = document.createElement("div");
+        idDiv.textContent = `Order #${order.orderID}`;
+        box.appendChild(idDiv);
+
+        const productsDiv = document.createElement("div");
+
+        order.products.forEach((products) => {
+          fetch("/api/product/" + products.pid)
+            .then((response) => response.json())
+            .then((product) => {
+              console.log(product);
+              const productDiv = document.createElement("div");
+              productDiv.textContent = product[0].name;
+              productsDiv.appendChild(productDiv);
+              const priceDiv = document.createElement("div");
+              priceDiv.textContent = "HKD $" + product[0].price;
+              productsDiv.appendChild(priceDiv);
+              const quantityDiv = document.createElement("div");
+              quantityDiv.textContent = "x" + products.quantity;
+              productsDiv.appendChild(quantityDiv);
+            });
+        });
+        box.appendChild(productsDiv);
+
+        const totalDiv = document.createElement("div");
+        totalDiv.className = "order-total";
+        totalDiv.innerHTML = `<b>Total:</b><br>HKD $${order.total}`;
+        box.appendChild(totalDiv);
+
+        const statusDiv = document.createElement("div");
+        statusDiv.className = "order-status";
+        statusDiv.innerHTML = `<b>Status:</b><br>${order.status}`;
+        box.appendChild(statusDiv);
+
+        ordersList.appendChild(box);
+      });
+    });
+}
