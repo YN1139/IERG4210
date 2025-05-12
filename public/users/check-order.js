ec908c3f78a7007060db10f269ff861ba33e912c
Y.N <yanhyng0227@gmail.com>
2025-05-12 12:45:09 +0800

    Update admin.html to require name input for adding categories, and modify member-panel.html and users/index.html to link to the correct check order page. Add new route in server.mjs for handling check order requests and API endpoint for retrieving specific orders by ID.

---
+++ public/users/check-order.js
@@ -0,0 +1,68 @@
+document.getElementById("check-order-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchOrders();
+});
+
+async function fetchOrders() {
+  const orderID = document.getElementById("order-id").value;
+  const response = await fetch("/api/orders/" + orderID, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": document.querySelector('input[name="_csrf"]').value,
+    },
+  });
+  if (!response.ok) {
+    alert("Failed to fetch order. Please try again.");
+    return;
+  }
+  const orders = await response.json();
+
+  const ordersList = document.getElementById("orders-container");
+  ordersList.innerHTML = "";
+
+  orders.forEach((order) => {
+    console.log(order);
+    const box = document.createElement("div");
+    box.className = "order-box";
+    const idDiv = document.createElement("div");
+    idDiv.textContent = `Order #${order.orderID}`;
+    box.appendChild(idDiv);
+    const userDiv = document.createElement("div");
+    userDiv.textContent = `User: ${order.user}`;
+    box.appendChild(userDiv);
+
+    const productsDiv = document.createElement("div");
+
+    order.products.forEach((products) => {
+      fetch("/api/product/" + products.pid)
+        .then((response) => response.json())
+        .then((product) => {
+          console.log(product);
+          const productDiv = document.createElement("div");
+          productDiv.textContent = product[0].name;
+          productsDiv.appendChild(productDiv);
+          const priceDiv = document.createElement("div");
+          priceDiv.textContent = "HKD $" + product[0].price;
+          productsDiv.appendChild(priceDiv);
+          const quantityDiv = document.createElement("div");
+          quantityDiv.textContent = "x" + products.quantity;
+          productsDiv.appendChild(quantityDiv);
+        });
+    });
+    box.appendChild(productsDiv);
+
+    const totalDiv = document.createElement("div");
+    totalDiv.className = "order-total";
+    totalDiv.innerHTML = `<b>Total:</b><br>HKD $${order.total}`;
+    box.appendChild(totalDiv);
+
+    const statusDiv = document.createElement("div");
+    statusDiv.className = "order-status";
+    statusDiv.innerHTML = `<b>Status:</b><br>${order.status}`;
+    box.appendChild(statusDiv);
+
+    ordersList.appendChild(box);
+  });
+}
