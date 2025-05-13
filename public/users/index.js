ee398d092564b6a9459c37ec39a88e12c1f86cef
Y.N <yanhyng0227@gmail.com>
2025-05-13 23:51:30 +0800

    Add updatePopUpBox function to notify users of recent feature updates and enhance user experience with a notification on page load.

--- public/users/index.js
+++ public/users/index.js
@@ -9,6 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     fetchAllProducts();
   }
+
+  updatePopUpBox();
+});
+
+async function updatePopUpBox() {
   window.alert(
     "Here are some minor features updates after the project submission: \n" +
       "13/5 Update:\n" +
@@ -16,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "Updated sql query (added ORDER BY orderID DESC LIMIT 5) for fetching the most recent 5 orders for users\n" +
       "Added a pop up box for the updates"
   );
-});
+}
 
 async function fetchBreadcrumb(catid = null) {
   fetch("/api/category/" + catid)
